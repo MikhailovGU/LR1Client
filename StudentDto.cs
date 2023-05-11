f21978a862db5d7cdc39a278c8648ef8a727a733
HORIzzO <59036617+HORIzzO@users.noreply.github.com>
2023-05-12 00:07:54 +0300

    initMGU

---
+++ StudentDto.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace stankin1client;
+
+public class StudentDto
+{
+    public int Id { get; set; }
+
+    public string Name { get; set; }
+
+    public string Surname { get; set; }
+
+    public string CityName { get; set; }
+
+    public string DegreeName { get; set; }
+
+    public string DepartmentName { get; set; }
+
+    public string JobName { get; set; }
+}
